a484e18e492429511425f266a3bbd237d44534af
fatemaony <Fatema77095@gmail.com>
2025-04-23 22:58:48 +0600

    fixed footer links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaFacebook, FaLinkedin, FaYoutube } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 import logoFooter from '../assets/logo-footer.png'; // Replace with your logo path
 
 const Footer = () => {
@@ -15,10 +16,46 @@ const Footer = () => {
                     <h1 className="text-xl font-bold">Law.BD</h1>
                 </div>
                 <ul className="flex justify-center space-x-6 my-4">
-                    <li className="hover:underline cursor-pointer">Home</li>
-                    <li className="hover:underline cursor-pointer">My-Bookings</li>
-                    <li className="hover:underline cursor-pointer">Blogs</li>
-                    <li className="hover:underline cursor-pointer">Contact Us</li>
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) =>
+                                isActive ? 'underline font-bold' : 'hover:underline'
+                            }
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/my-bookings"
+                            className={({ isActive }) =>
+                                isActive ? 'underline font-bold' : 'hover:underline'
+                            }
+                        >
+                            My-Bookings
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/blogs"
+                            className={({ isActive }) =>
+                                isActive ? 'underline font-bold' : 'hover:underline'
+                            }
+                        >
+                            Blogs
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/contact-us"
+                            className={({ isActive }) =>
+                                isActive ? 'underline font-bold' : 'hover:underline'
+                            }
+                        >
+                            Contact Us
+                        </NavLink>
+                    </li>
                 </ul>
                 <hr className="border-dashed border-gray-500 my-4 w-1/2 mx-auto" />
                 <div className="flex justify-center space-x-6">
@@ -32,3 +69,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
